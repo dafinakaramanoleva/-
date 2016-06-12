bb2913af6601b2533820fc8a5760d6dd8bebd27a
Simeon <simeon.andreev89@gmail.com>
2016-06-13 00:41:50 +0300

    Web design - music category

--- js/main.js
+++ js/main.js
@@ -35,4 +35,30 @@ $(function(){
 		$('#user_name').html($("input[name=user_name]").val())
 	})
 
+	//Increments
+	$(".temp-button").on("click", function() {
+
+	  var $button = $(this);
+	  var oldValue = $button.parent().find("input").val();
+
+	  if ($button.text() == "+") {
+		  var newVal = parseFloat(oldValue) + 1;
+		} else {
+	   // Don't allow decrementing below zero
+	    if (oldValue > 0) {
+	      var newVal = parseFloat(oldValue) - 1;
+	    } else {
+	      newVal = 0;
+	    }
+	  }
+
+	  $button.parent().find("input").val(newVal);
+
+	});
+
+	//Volum Controll
+	$('.on-off-wrapper > div').on('click', function(){
+		$(this).toggleClass('on');
+	});
+
 });
